313a195e4724635f02967d180e11191f845fec16
peturik <peturik@gmail.com>
2026-08-02 16:20:52 +0200

    updated install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 mkdir -p ~/.config/alacritty/
 mkdir -p ~/.config/nvim/
-ln -sf "$HOME".dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-ln -sf "$HOME".dotfiles/tmux/.tmux.conf ~/.tmux.conf
-ln -sf "$HOME".dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf "$HOME"/.dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sf "$HOME"/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sf "$HOME"/.dotfiles/zsh/.zshrc ~/.zshrc
 # ln -sf "$HOME".dotfiles/starship/starship.toml ~/.config/starship.toml
-ln -sf "$HOME".dotfiles/lazyvim/nvim ~/.config/nvim
+ln -sf "$HOME"/.dotfiles/lazyvim/nvim ~/.config/nvim
 
 echo "Dotfiles installed!"
 
