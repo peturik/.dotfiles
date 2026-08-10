ea4e35bedb7ba4381b751851a9d7b2d352e52d2e
peturik <peturik@gmail.com>
2026-08-10 09:19:15 +0200

    add diagnostc preview

--- nvchad/nvim/lua/mappings.lua
+++ nvchad/nvim/lua/mappings.lua
@@ -9,4 +9,15 @@ map("i", "jk", "<ESC>")
 map("n", "<leader>w", "<cmd> w <cr>", { desc = "save file" })
 -- map("n", "<leader>q", "<cmd> wq <cr>", { desc = "exit" })
 
+map("n", "<leader>sd", function()
+  vim.diagnostic.open_float { border = "rounded", float = true }
+end, { desc = "Show full diagnostic in a floating window" })
+-- Перехід до наступного/попереднього
+map("n", "]d", function()
+  vim.diagnostic.jump { count = 1, float = true }
+end, { desc = "Next diagnostic" })
+
+map("n", "[d", function()
+  vim.diagnostic.jump { count = -1, float = true }
+end, { desc = "Previous diagnostic" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
